3e0edf9a59a8442ef825f2a81643c808961d0c09
kyam <kou.skillup@gmail.com>
2017-09-03 00:45:45 +0900

    undo操作（push pop）プログラム完了

--- undo.js
+++ undo.js
@@ -1,20 +1,19 @@
 
 //html_str=getcurentcontent();
-html_str="jfa;lksdjf;lamsd;nasv;lkajsf;lsadkf"
-/*
-var undo = new Object();
-undo.stack = [];
-*/
-function undo(html_str){
+
+var html_str="jfa;lksdjf;lamsd;nasv;lkajsf;lsadkf";
+
+function undo(){
 	this.stack=[]
 	this.htmlstr=""
 	this.stack_push=function(html_str){
-		if(stack.length==11){
-			stack.shift()
-			stack.push(html_str)
+		if(this.stack.length>11){
+			this.stack.shift()
+			//stackの最初を削除
+			this.stack.push(html_str)
 		}
 		else{
-			stack.push(html_str)
+			this.stack.push(html_str)
 		}
 	}
 	this.stack_pop=function(){
@@ -22,43 +21,8 @@ function undo(html_str){
 	}
 }
 
-var example = undo(html_str)
-var respoense = undo.stack_pop()
+var example = new undo();
+example.stack_push(html_str);
+var respoense = example.stack_pop();
+
 console.log(respoense);
-
-/*
-var stack=[];
-
-function stack_push(html_str){
-
-}
-
-function undo(){
-		return stack.pop()
-}
-*/
-
-
-/*
-var undoManager = {
-	undoStack : [],
-	register : function ( fn, html_str ) {
-		this.undoStack.push ( { 'fn' : fn, 'html_str' :html_str  } );
-	},
-	undo : function () {
-		if ( this.undoStack.length > 0 ) {
-			var undoItem = this.undoStack [ this.undoStack.length -1 ];
-			this.undoStack.pop ();
-			undoItem.fn ( undoItem.html_str );
-			this.undoStack.pop ();
-		};
-	}
-};
-
-
-function show ( arg ) { //【12】
-	var element = document.getElementById ( arg.id ); //【13】
-	element.style.visibility = arg.showing ? 'visible' : 'hidden'; //【14】
-	undoManager.register ( show, { id: arg.id, showing: !arg.showing },); //【15】
-};
-*/
