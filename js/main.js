6e76410c8d387e5395ebdd07e48561c2b5652326
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2017-09-03 02:20:09 +0900

    一つしかsummernoteを開けないように修正

--- js/main.js
+++ js/main.js
@@ -27,56 +27,46 @@ $( function() {
     $sortable.droppable({
         tolerance: "intersect",
         drop: function( event, ui ) {
-            // $( this )
-            // .addClass( "ui-state-highlight" )
-            // .find( "p" )
-            // .html( "Dropped!" );
-            // console.log(ui.draggable.text());
 
         }
     });
 
+    /**
+     * 画面左側の要素のidを取得して、それに応じたhtmlタグを返す
+     * @param  {[type]} item   左側の項目の名前
+     * @param  {[type]} _event dragイベント
+     * @return {[string]}      html
+     */
     var getItemHtml = function(item, _event){
         var _html;
         var _left = "left:" + _clientX + "px;";
         var _top = "top:" + _clientY + "px;";
         var _style =  _position + _left + _top;
         if(item.attr("id") === "item_text"){
-            _html = "<p class='textform' style='"+ _style +"'>text</p>";
+            _html = "<div class='textform' style='"+ _style +"'>text</div>";
         }
         return _html;
     }
 
 
-    // p -> input
+    /**
+     * サマーノートを開く、saveButtonの作成
+     * @return {[type]} [description]
+     */
     $(document).on("click", ".textform", function(){
-        _clientX = $(this).css("left");
-        _clientY = $(this).css("top");
 
-        var text = $(this).text();
-        var _left = "left:" + _clientX + ";";
-        var _top = "top:" + _clientY + ";";
-        var _style =  _position + _left + _top;
-        $(this).replaceWith("<div class='textform' style='"+ _style +"'>"+text+"</div>");
-
-        _input = $(".textform").summernote({focus: true});
-        // _input = $(this);
-        var saveButton = "<div class='formSave' style='display:block;padding-top:15px;padding-bottom:15px;background-color:#4499ff;width:70px;text-align:center;border:1px solid #2222ff'>save</div>"
-        $sortable.append(saveButton);
+        if(!_input){
+            _input = $(this).summernote({focus: true});
+            // _input = $(this);
+            var saveButton = "<div class='formSave' style='display:block;padding-top:15px;padding-bottom:15px;background-color:#4499ff;width:70px;text-align:center;border:1px solid #2222ff'>save</div>"
+            $sortable.append(saveButton);
+        }
     });
-
-    // text click時の処理
-    // $(document).on("click", "._textform", function(){
-    //     $(this).summernote({focus: true});
-    //
-    //     _input = $(this);
-    //     var saveButton = "<div class='formSave' style='display:block;padding-top:15px;padding-bottom:15px;background-color:#4499ff;width:70px;text-align:center;border:1px solid #2222ff'>save</div>"
-    //     $sortable.append(saveButton);
-    // });
 
     $(document).on("click", ".formSave", function(){
         _input.summernote('destroy');
         $(this).remove();
+        _input = undefined;
     });
 
 
