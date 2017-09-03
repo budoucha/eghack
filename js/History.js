44f8a23fa84fe0b9821ee4756a9e1717959b686e
Haruto Kanno <budoucha@gmail.com>
2017-09-03 15:16:25 +0900

    テスト用のコードが残っていたので削除

--- js/History.js
+++ js/History.js
@@ -1,17 +1,13 @@
 function History() {
     this.undo_stack = [];
     this.redo_stack = [];
-    this.cnt=0;
     this.stack_push = function () {
-        var current_ws = this.cnt;//getCurrentContentHTML();
-        this.cnt++;
+        var current_ws = getCurrentContentHTML();
         if (this.undo_stack.length > 10) { // 古い履歴の削除
             this.undo_stack.shift();
         }
         this.undo_stack.push(current_ws);
         this.redo_stack = [];
-        console.log(this.undo_stack);
-        console.log(this.redo_stack);
     }
 
     this.undo = function () {
@@ -22,8 +18,6 @@ function History() {
         if (this.redo_stack.length > 11) {
             this.redo_stack.shift();
         }
-        console.log(this.undo_stack);
-        console.log(this.redo_stack);
         return this.htmlstr_res;
     }
     this.redo = function () {
@@ -31,7 +25,5 @@ function History() {
             this.htmlstr_res = this.redo_stack.pop();
             this.undo_stack.push(this.htmlstr_res);
         }
-        console.log(this.undo_stack);
-        console.log(this.redo_stack);
     }
 }
